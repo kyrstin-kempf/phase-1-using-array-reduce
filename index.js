2328a2c9514f12363e475bc3437a786cd5c02586
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-02-21 17:28:15 -0500

    commit tree b95e621ce67515bbe2f625f5103e3d13ac55d9f5

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const totalBatters = batteryBatches.reduce(callbackFn [, 0])
+
+let totalBatteries = batteryBatches.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+});
